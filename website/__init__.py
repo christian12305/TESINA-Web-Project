cf7bb4ffe22fe2b8168ff944cba31e3e0c764db3
Christian Ramos <christian.ramos24@upr.edu>
2023-09-29 21:05:16 -0400

    Started development of UI

--- website/__init__.py
+++ website/__init__.py
@@ -1,21 +1,49 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from os import path
 from flask_login import LoginManager
+from flask_mysqldb import MySQL
+
+db = SQLAlchemy()
+DB_NAME = "database.db"
+
 
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'TESINA2023S1'
+    app.config['SECRET_KEY'] = 'hjshjhdjah kjshkjdhjs'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
 
+    #app.config['MYSQL_HOST'] = 'localhost'
+    #app.config['MYSQL_USER'] = 'root'
+    #app.config['MYSQL_PASSWORD'] = ''
+    #app.config['MYSQL_DB'] = 'flask'
 
+    #db = MySQL(app)
+    ##Creating a connection cursor
+    #cursor = mysql.connection.cursor()
 
+    #Executing SQL Statements
+    #cursor.execute(''' CREATE TABLE table_name(field1, field2...) ''')
+    #cursor.execute(''' INSERT INTO table_name VALUES(v1,v2...) ''')
+    #cursor.execute(''' DELETE FROM table_name WHERE condition ''')
 
-    #blueprints
+    #Saving the Actions performed on the DB
+    #mysql.connection.commit()
+    #Closing the cursor
+    #cursor.close()
+
+    db.init_app(app)
+
     from .views import views
     from .auth import auth
 
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
 
-    from .models import User
+    from .models import User, Note
+    
+    with app.app_context():
+        db.create_all()
 
     login_manager = LoginManager()
     login_manager.login_view = 'auth.login'
@@ -26,3 +54,9 @@ def create_app():
         return User.query.get(int(id))
 
     return app
+
+
+def create_database(app):
+    if not path.exists('website/' + DB_NAME):
+        db.create_all(app=app)
+        print('Created Database!')
