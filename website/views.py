a169b224c53951212607a7fbdd63700a8072576f
Christian Ramos <christian.ramos24@upr.edu>
2023-09-30 12:57:14 -0400

    Added a Profile page

--- website/views.py
+++ website/views.py
@@ -1,16 +1,19 @@
-from flask import Blueprint, render_template, request, flash, jsonify, redirect, url_for
-from flask_login import login_required, current_user
+from flask import Blueprint, render_template, request, flash, jsonify, redirect, url_for, session
+from flask_login import current_user
 from .models import Patient
 from . import db
 import json
+import MySQLdb.cursors
+
 
 views = Blueprint('views', __name__)
 
 
 @views.route('/', methods=['GET', 'POST'])
-@login_required
 def home():
-    if request.method == 'POST': 
+    if 'loggedin' in session:
+
+        if request.method == 'POST': 
         #note = request.form.get('note')#Gets the note from the HTML 
 
         #if len(note) < 1:
@@ -20,53 +23,71 @@ def home():
             #db.session.add(new_note) #adding the note to the database 
             #db.session.commit()
             #flash('Note added!', category='success')
-        pass
+            pass
+        
 
-    return render_template("home.html", user=current_user)
+        return render_template("home.html", user=session['username'])
+    return redirect(url_for('auth.login'))
 
 
 @views.route('/search', methods=['GET', 'POST'])
-@login_required
 def search_patient():
-    if request.method == 'POST':
-        #Received a search request
-        pass
-    return render_template("search_patient.html", user=current_user)
+    if 'loggedin' in session:
+
+        if request.method == 'POST':
+            #Received a search request
+            pass
+        return render_template("search_patient.html", user=session['username'])
+    return redirect(url_for('auth.login'))
 
 
 @views.route('/create', methods=['GET', 'POST'])
-@login_required
 def create_patient():
-    if request.method == 'POST':
-        #Patient has been submitted for creation
-        firstName = request.form.get('firstName')
-        intial = request.form.get('initial')
-        lastName = request.form.get('lastName')
-        gender = request.form.get('gender')
-        weight = request.form.get('weight')
-        conditon = request.form.get('condition')
-        #patient = Patient.query.filter_by(firstName=firstName, intial=intial, lastName=lastName).first()
-        #if patient:
-            #Conditions to be met
-            #elif
-            #elif
-            #else
-            #if all conditions are met, create patient
-        #    pass
-        return redirect(url_for('views.patient_record'))
-        #return render_template("patient_record.html", user=current_user)
+    if 'loggedin' in session:
+
+        if request.method == 'POST':
+            #Patient has been submitted for creation
+            firstName = request.form.get('firstName')
+            intial = request.form.get('initial')
+            lastName = request.form.get('lastName')
+            gender = request.form.get('gender')
+            weight = request.form.get('weight')
+            conditon = request.form.get('condition')
+            #patient = Patient.query.filter_by(firstName=firstName, intial=intial, lastName=lastName).first()
+            #if patient:
+                #Conditions to be met
+                #elif
+                #elif
+                #else
+                #if all conditions are met, create patient
+            #    pass
+            return redirect(url_for('views.patient_record'))
+            #return render_template("patient_record.html", user=current_user)
 
         
-    return render_template("create_patient.html", user=current_user)
+        return render_template("create_patient.html", user=current_user)
+    return redirect(url_for('auth.login'))
+
+
 
 
 @views.route('/patient-record', methods=['GET', 'POST'])
-@login_required
 def patient_record():
-    if request.method == 'GET':
-        return render_template("patient_record.html", user=current_user)
-        #return render_template("patient_record.html", patient=patient)
+    if 'loggedin' in session:
+        if request.method == 'GET':
+            return render_template("patient_record.html", user=current_user)
+            #return render_template("patient_record.html", patient=patient)
+    return redirect(url_for('auth.login'))
 
+
+@views.route('/profile', methods=['GET', 'POST'])
+def profile():
+    if 'loggedin' in session:
+        cursor = db.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM USUARIO WHERE id_pk = %s', (session['id'],))
+        user = cursor.fetchone()
+        return render_template("profile.html", user)
+    return redirect(url_for('auth.login'))
 
 
 
