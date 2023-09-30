aa840a626a1ffe5316726a765530f2dfb2c20d3b
Christian Ramos <christian.ramos24@upr.edu>
2023-09-29 22:06:51 -0400

    Added views to transition between pages

--- website/models.py
+++ website/models.py
@@ -3,11 +3,14 @@ from flask_login import UserMixin
 from sqlalchemy.sql import func
 
 
-class Note(db.Model):
+class Patient(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    data = db.Column(db.String(10000))
-    date = db.Column(db.DateTime(timezone=True), default=func.now())
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    lastName = db.Column(db.String(30))
+    initial = db.Column(db.String(2))
+    firstName = db.Column(db.String(30))
+    gender = db.Column(db.String(10))
+    weight = db.Column(db.String(4))
+    condition = db.Column(db.String(20))
 
 
 class User(db.Model, UserMixin):
@@ -15,4 +18,4 @@ class User(db.Model, UserMixin):
     email = db.Column(db.String(150), unique=True)
     password = db.Column(db.String(150))
     first_name = db.Column(db.String(150))
-    notes = db.relationship('Note')
+    #notes = db.relationship('Note')
--- website/views.py
+++ website/views.py
@@ -1,6 +1,6 @@
-from flask import Blueprint, render_template, request, flash, jsonify
+from flask import Blueprint, render_template, request, flash, jsonify, redirect, url_for
 from flask_login import login_required, current_user
-from .models import Note
+from .models import Patient
 from . import db
 import json
 
@@ -11,19 +11,71 @@ views = Blueprint('views', __name__)
 @login_required
 def home():
     if request.method == 'POST': 
-        note = request.form.get('note')#Gets the note from the HTML 
+        #note = request.form.get('note')#Gets the note from the HTML 
 
-        if len(note) < 1:
-            flash('Note is too short!', category='error') 
-        else:
-            new_note = Note(data=note, user_id=current_user.id)  #providing the schema for the note 
-            db.session.add(new_note) #adding the note to the database 
-            db.session.commit()
-            flash('Note added!', category='success')
+        #if len(note) < 1:
+        #   flash('Note is too short!', category='error') 
+        #else:
+            #new_note = Note(data=note, user_id=current_user.id)  #providing the schema for the note 
+            #db.session.add(new_note) #adding the note to the database 
+            #db.session.commit()
+            #flash('Note added!', category='success')
+        pass
 
     return render_template("home.html", user=current_user)
 
 
+@views.route('/search', methods=['GET', 'POST'])
+@login_required
+def search_patient():
+    if request.method == 'POST':
+        #Received a search request
+        pass
+    return render_template("search_patient.html", user=current_user)
+
+
+@views.route('/create', methods=['GET', 'POST'])
+@login_required
+def create_patient():
+    if request.method == 'POST':
+        #Patient has been submitted for creation
+        firstName = request.form.get('firstName')
+        intial = request.form.get('initial')
+        lastName = request.form.get('lastName')
+        gender = request.form.get('gender')
+        weight = request.form.get('weight')
+        conditon = request.form.get('condition')
+        #patient = Patient.query.filter_by(firstName=firstName, intial=intial, lastName=lastName).first()
+        #if patient:
+            #Conditions to be met
+            #elif
+            #elif
+            #else
+            #if all conditions are met, create patient
+        #    pass
+        return redirect(url_for('views.patient_record'))
+        #return render_template("patient_record.html", user=current_user)
+
+        
+    return render_template("create_patient.html", user=current_user)
+
+
+@views.route('/patient-record', methods=['GET', 'POST'])
+@login_required
+def patient_record():
+    if request.method == 'GET':
+        return render_template("patient_record.html", user=current_user)
+        #return render_template("patient_record.html", patient=patient)
+
+
+
+
+
+
+
+
+
+'''
 @views.route('/delete-note', methods=['POST'])
 def delete_note():  
     note = json.loads(request.data) # this function expects a JSON from the INDEX.js file 
@@ -35,3 +87,4 @@ def delete_note():
             db.session.commit()
 
     return jsonify({})
+'''
